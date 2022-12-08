9fae850f05926b4b1b94af9b10b65f2ee9ee09b9
ravansamarth <ravansamarth@gmail.com>
2022-12-08 17:34:06 +0530

    second push

--- interest.py
+++ interest.py
@@ -1,1 +1,31 @@
-print("hello world")
+print("HERE'S YOUR INTEREST")
+
+while True:
+    print("\nChoose 1:")
+    print("1.SI")
+    print("2.CI")
+    print("3.Exit")
+    
+    choice = int(input("\nEnter Choice: "))
+
+    if choice == 1:
+        P = float(input('Enter amount: '))
+        t = float(input('Enter time in years: '))
+        r = float(input('Enter roi: '))
+        SI = (P*t*r)/100
+        print('Simple interest is: %f' % (SI))
+        
+        
+    elif choice == 2:
+        P = float(input('Enter amount: '))
+        t = float(input('Enter time in years: '))
+        r = float(input('Enter roi: '))
+        CI = P * ( (1+r/100)**t - 1)
+        print('Compound interest is: %f' %(CI))
+
+    elif choice == 3:
+        print("ThankS !")
+        break
+    
+    else:
+        print("Invalid Input!")
